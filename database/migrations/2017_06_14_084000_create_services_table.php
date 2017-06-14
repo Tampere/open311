6b310fa1b9bbdb49d57be41be39e73157553bd17
Antti Sand <antti.sand@tampere.fi>
2017-06-14 11:54:37 +0300

    Initial model schemas

---
+++ database/migrations/2017_06_14_084000_create_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->uuid('service_code');
+            $table->string('service_name');
+            $table->text('description');
+            $table->text('group');
+            $table->timestamps();
+
+            $table->primary('service_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
---
+++ database/migrations/2017_06_14_084011_create_requests_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->uuid('service_request_id');
+            $table->uuid('service_code');
+            $table->string('status')->default('open');
+            $table->text('status_notes')->nullable();
+            $table->string('agency_responsible')->nullable();
+            $table->text('description');
+            $table->string('title')->nullable();
+            $table->string('address_string')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('email')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('media_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('service_code')
+                ->references('service_code')
+                ->on('services')
+                ->onDelete('cascade');
+
+            $table->primary('service_request_id');
+        });
+
+        DB::statement('ALTER TABLE requests ADD location POINT' );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
