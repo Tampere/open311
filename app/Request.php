1e573c991a40b9f281335b7dc20de8cd141dd18b
Antti Sand <antti.sand@tampere.fi>
2017-06-15 09:42:11 +0300

    Fixed wrong use statement

--- app/Request.php
+++ app/Request.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Filters\RequestFilters;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
@@ -14,4 +16,9 @@ class Request extends Model
     public $primaryKey = 'service_request_id';
 
     protected $guarded = [];
+
+    public function scopeFilter(Builder $query, RequestFilters $filters)
+    {
+        return $filters->apply($query);
+    }
 }
