7e97615192ba9437b6f47fa993c0d7ff501bee03
Antti Sand <antti.sand@tampere.fi>
2017-06-16 10:16:55 +0300

    Test posting to request endpoint

--- tests/Feature/RequestsTest.php
+++ tests/Feature/RequestsTest.php
@@ -17,7 +17,19 @@ class RequestsTest extends TestCase
         $service = factory('App\Service')->create();
         $request = factory('App\Request')->make(['service_code' => $service->service_code]);
 
-        $this->post('requests.json', $request->toArray())
-            ->assertStatus(200);
+        $this->post('v1/requests.json', [
+            'service_code' => $service->service_code,
+            'description' => $request->description,
+            'title' => $request->title,
+            'lat' => 25.11,
+            'long' => 17.643435,
+            'address_string' => $request->address_string,
+            'zip_code' => $request->zip_code,
+            'email' => $request->email,
+            'fist_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'phone' => $request->phone,
+            'media_url' => $request->media_url,
+        ])->assertStatus(200);
     }
 }
