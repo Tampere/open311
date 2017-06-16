48a615b48ebcbf4343c37bc7b78be408665cc9bd
Antti Sand <antti.sand@tampere.fi>
2017-06-16 11:49:19 +0300

    Routes for UI

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', function() {
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/services', 'HomeController@index')->name('services');
+Route::get('/requests', 'HomeController@index')->name('requests');
