d067fb415fa200c60260a0b99857d740711678bd
Antti Sand <antti.sand@tampere.fi>
2017-06-16 10:15:13 +0300

    Resources controller first pass

---
+++ app/Http/Controllers/RequestsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Filters\RequestFilters;
+use App\Http\Requests\ServiceRequest;
+use App\Request;
+use App\Transformers\ServiceRequestTransformer;
+
+class RequestsController extends Controller
+{
+    /**
+     * @param $extension
+     * @param RequestFilters $filters
+     * @return \Spatie\Fractal\Fractal
+     */
+    public function index($extension, RequestFilters $filters)
+    {
+        $this->checkExtension($extension);
+
+        // Providing service_request_id overrides all other parameters
+        if(request()->has('service_request_id')) {
+            return $this->showMany(request()->get('service_request_id'), $extension);
+        }
+
+        $requests = Request::with('service')->latest();
+
+        // If all lat, long and radii are present, we can calculate distance. Individually each are worthless.
+        if(request()->has(['lat', 'long', 'radius'])) {
+            $requests->distance(
+                request()->get('radius'),
+                sprintf("%f,%f",
+                    request()->get('lat'),
+                    request()->get('long')
+                )
+            );
+        }
+
+        $requests->filter($filters);
+
+        return fractal($requests->get(), new ServiceRequestTransformer);
+    }
+
+    /**
+     * @param $service_request_id
+     * @param $extension
+     * @return \Spatie\Fractal\Fractal
+     */
+    public function show($service_request_id, $extension)
+    {
+        $this->checkExtension($extension);
+
+        $request = Request::findOrFail($service_request_id);
+
+        return fractal($request, new ServiceRequestTransformer);
+    }
+
+    /**
+     * @param ServiceRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(ServiceRequest $request)
+    {
+        $serviceRequest = $request->persist();
+
+        return response()->json([
+            'service_request_id' => $serviceRequest->service_request_id,
+            'service_notice' => '',
+        ], 200);
+    }
+
+    /**
+     * @param $service_request_id
+     * @param $extension
+     * @return \Spatie\Fractal\Fractal
+     */
+    private function showMany($service_request_id, $extension)
+    {
+        if(strpos($service_request_id, ',')) {
+            $ids = explode(',', $service_request_id);
+        } else {
+            $ids = $service_request_id;
+        }
+
+        $request = Request::whereIn('service_request_id', $ids)->get();
+
+        return fractal($request, new ServiceRequestTransformer);
+    }
+}
