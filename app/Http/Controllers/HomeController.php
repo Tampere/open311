02a2e5213a0489c985d4277f8f2ec42588dbe014
Antti Sand <antti.sand@tampere.fi>
2017-06-21 09:44:27 +0300

    User handling

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
     /**
@@ -12,5 +15,39 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function profile()
+    {
+        return view('users.show', ['user' => auth()->user()]);
+    }
+
+    public function userslist()
+    {
+        $users = User::all();
+
+        return view('users.index', ['users' => $users]);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,'.$user->id,
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user->update($request->only(['name', 'email', 'password']));
+
+        return redirect('users')
+            ->with('status', 'User updated.');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect('users')
+            ->with('status', 'User deleted.');
     }
 }
