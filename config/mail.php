ac488e0966b1a4cb595e925983d6b0f54e06e3f9
Antti Sand <antti.sand@tampere.fi>
2017-10-18 15:12:40 +0200

    Set correct sender info

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'palaute@tampere.fi'),
+        'name' => env('MAIL_FROM_NAME', 'Konsernipalveluyksikkö Koppari'),
     ],
 
     /*
